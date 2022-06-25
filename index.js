da6ceaa43a2716cf3c9ca7cc1c671235a77b6848
Tusharv0607 <2020110785@diu.iiitvadodara.ac.in>
2022-06-25 11:51:10 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.get('/', function (req, res) {
  res.send("Hello World")
-})
+});
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
 app.listen(process.env.PORT || port, () => {
   console.log(`iNotebook listening on port ${port}`)
-})
+});
